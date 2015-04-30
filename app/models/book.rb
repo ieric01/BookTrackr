58a754d0f8601d81659a91d30a08ce87f3d92694
Eric Orellana <eric.orellana92@gmail.com>
2015-04-29 20:09:42 -0400

    initial commit with books model and migration set

---
+++ app/models/book.rb
@@ -0,0 +1,7 @@
+class Book < ActiveRecord::Base
+	belongs_to :User
+
+	def index
+	end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  
+  #set the books index page to be the root of app
+  root 'books#index'
+
+end
